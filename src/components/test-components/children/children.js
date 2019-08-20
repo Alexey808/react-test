0090e193fcaebc155324f59bf40ef2cbcb336abc
Alexey808 <laiten808@gmail.com>
2019-08-20 22:31:06 +0400

    child view

--- src/components/test-components/children/children.js
+++ src/components/test-components/children/children.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 export class WrapperComponent extends React.Component {
 	render() {
 		const {children} = this.props;
@@ -9,13 +11,43 @@ export class WrapperComponent extends React.Component {
 
 export default class Children extends React.Component {
 	render () {
+
+		const item = {id: 404, prop: 'prop404'};
 		return (
 			<div>
 				<WrapperComponent>
 					{'test'}
 				</WrapperComponent>
+
+				<WrapperComponent2 item={item}>
+					<ItemDetails id="id" prop="prop"/>
+				</WrapperComponent2>
 			</div>
 		);
 	}
-
 }
+
+export class WrapperComponent2 extends React.Component {
+
+	render() {
+		const {item} = this.props;
+		return(
+			<div>
+				{
+					React.Children.map(this.props.children, (child) => {
+						return React.cloneElement(child, {item});
+					})
+				}
+			</div>
+		);
+	}
+}
+
+const ItemDetails = ({item, id, prop}) => {
+	return(
+		<ul>
+			<li>{item[id]}</li>
+			<li>{item[prop]}</li>
+		</ul>
+	);
+};
